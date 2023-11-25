c2582fa4bb4a188674420bc2365b233426f06be9
thiago <thiagovakdez@gmail.com>
2023-11-25 18:03:38 -0300

    subo pryecto base 2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Encabezado from './Encabezado';
+
+function App() {
+  return (
+    <div className="App">
+      <Encabezado />
+      <main>
+        {/* Contenido principal de la página */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Encabezado.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Encabezado.css';
+
+function Encabezado() {
+  return (
+    <header>
+      <img src="https://i.ibb.co/Ct4GtpW/GUITARRALIBRE-1-removebg-preview-3.png" alt="GUITARRALIBRE"/>
+      <nav>
+      <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Sobre Nosotros</a></li>
+      </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Encabezado;
