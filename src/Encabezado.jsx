b6505e37e90bf2ea74e4acd5b57f68626c5ad470
thiago <thiagovakdez@gmail.com>
2023-11-27 06:06:41 -0300

    agrego funcion para nav a medias, commiteo por las dudas

--- src/Encabezado.jsx
+++ src/Encabezado.jsx
@@ -1,18 +1,33 @@
 // encabezado.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Encabezado.css';
 
 function Encabezado() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const handleInicioClick = () => {
     window.location.reload();
+  };
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+    // Puedes agregar aquí la lógica para filtrar los productos por nombre
+    // y actualizar el estado de la lista de productos en el componente App.
   };
 
   return (
     <header>
       <img src="https://i.ibb.co/Ct4GtpW/GUITARRALIBRE-1-removebg-preview-3.png" alt="GUITARRALIBRE" />
       <form action="#" method="get">
-        <input type='text' name='search' placeholder='Buscar en la página'></input>
+        <input
+          type='text'
+          name='search'
+          placeholder='Buscar en la página'
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
         <button type='submit' name='Buscar'> </button>
       </form>
 
